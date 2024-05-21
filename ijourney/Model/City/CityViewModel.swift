afbb6360ed734a41abe7bb6b6b6ecf6247fd0a7f
Lambo Zhuang <Lambozhuang@gmail.com>
2024-05-22 01:00:56 +0200

    remove unused method

--- ijourney/Model/City/CityViewModel.swift
+++ ijourney/Model/City/CityViewModel.swift
@@ -10,27 +10,5 @@ import Foundation
 @MainActor
 class CityViewModel: ObservableObject {
   @Published var allCities: [City] = []
-  
-  func loadCities() {
-    guard let url = Bundle.main.url(forResource: "country-by-cities", withExtension: "json"),
-          let data = try? Data(contentsOf: url) else {
-      print("Failed to load file")
-      return
-    }
-    
-    do {
-      let jsonCountries = try JSONSerialization.jsonObject(with: data, options: []) as? [[String: Any]]
-      var citiesArray: [City] = []
-      for countryData in jsonCountries ?? [] {
-        if let countryName = countryData["country"] as? String,
-           let cityNames = countryData["cities"] as? [String] {
-          let cities = cityNames.map { City(name: $0, countryName: countryName) }
-          citiesArray.append(contentsOf: cities)
-        }
-      }
-      allCities = citiesArray
-    } catch {
-      print("Failed to decode JSON: \(error)")
-    }
-  }
+
 }
